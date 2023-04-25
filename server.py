d6860c08a86b9aa6fdfdec2557a45f239b70ce18
Elidaaa <114477389+Elidaaa@users.noreply.github.com>
2023-04-25 16:29:01 +0200

    ajout de la fonction sign_in et sign_out afin de connecter/deconnecter l'utilisateur via une verification sur la bdd

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import logging
 import logging.handlers
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, redirect, url_for, session
 from jinja2 import TemplateNotFound
-
+import hashlib
 import interfaces
+import mysql as mysql
+import mysql.connector
+from entity.User import User
 
 
 class Server(interfaces.Server_interface):
@@ -20,6 +23,7 @@ class Server(interfaces.Server_interface):
         self.__logger.addHandler(fh)
         self.__logger.setLevel('DEBUG')
         logging.root.handlers = [fh]
+        self.connected_user = None
         
 
     @property
@@ -39,13 +43,24 @@ class Server(interfaces.Server_interface):
 
         self.__logger.debug("Creating app object")
         self.__app = Flask("The Coral Planters", template_folder='./templates', static_folder='./static')
+        self.__app.config['SECRET_KEY'] = 'secret_key'
+        self.__app.config['SESSION_TYPE'] = 'filesystem'
+
         self.__logger.debug("App object created")
 
         self.app.add_url_rule("/", "index", self.index)
-        self.app.add_url_rule("/sign_in", "sign in", self.sign_in)
+        self.app.add_url_rule("/sign_in", "sign in", self.sign_in, methods=["GET", "POST"])
         self.app.add_url_rule("/sign_up", "sign up", self.sign_up)
+        self.app.add_url_rule("/logout", "logout", self.logout)
         self.app.add_url_rule("/upload", "upload", self.upload)
         self.app.add_url_rule("/coral_info", "Show coral info", self.coral_info)
+        self.conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="reefscapers2020_v3"
+        )
+        self.cursor = self.conn.cursor()
         return self.__app
 
     def run_test_server(self):
@@ -102,12 +117,37 @@ class Server(interfaces.Server_interface):
             abort(404)
     
     def sign_in(self):
-        """TO BE MODIFIED TO HANDLE BOTH POST AND GET REQUESTS"""
 
-        try:
-            return render_template("sign_in.html")
-        except TemplateNotFound:
-            abort(404)
+        self.__logger.info("Running server in debug mode...")
+
+        if request.method == 'POST':
+            # Do something with the submitted form data
+            username = request.form['username']
+            password = request.form['password']
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            print("username : ", username, " password : ", hashed_password)
+            try:
+                sql_query = "SELECT id, username, role, email, fragment_id FROM utilisateurs WHERE username = %s AND password = %s"
+                params = (username, hashed_password)
+                self.cursor.execute(sql_query, params)
+                res = self.cursor.fetchall()
+                self.connected_user = User(res[0][0], res[0][1], res[0][2], res[0][3], res[0][4])
+                session['logged_in'] = True
+
+                print(self.connected_user)
+                self.conn.close()
+
+                return redirect('/')
+            except:
+                return redirect('/sign_in')
+        elif request.method == 'GET':
+            try:
+                
+                return render_template("sign_in.html")
+            except TemplateNotFound:
+                abort(404)
+
+
 
     def sign_up(self):
         """TO BE MODIFIED TO HANDLE BOTH POST AND GET REQUESTS"""
@@ -140,4 +180,9 @@ class Server(interfaces.Server_interface):
             return render_template("coral_info.html")
         except TemplateNotFound:
             abort(404)
-
+    
+    def logout(self):
+        self.connected_user = None
+        session['logged_in'] = None
+        return redirect('/')
+    
