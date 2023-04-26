a15a7143c2048ac31ec33cb7af866e3bf2e69924
Lakhbir <lakhbirs085@gmail.com>
2023-04-26 14:05:39 +0200

    test of the script unittest website

---
+++ test_website.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import patch, Mock
+from server import Server
+
+
+class TestServer(unittest.TestCase):
+
+    def setUp(self):
+        self.server = Server(logfile="test.log")
+        
+    def tearDown(self):
+        pass
+        
+            
+    def test_index(self):
+        with patch("server.render_template") as mock_render_template:
+            response = self.server.index()
+            self.assertIsNotNone(response)
+            self.assertTrue(mock_render_template.called)
+            
+    def test_sign_in(self):
+        with patch("server.render_template") as mock_render_template:
+            response = self.server.sign_in()
+            self.assertIsNotNone(response)
+            self.assertTrue(mock_render_template.called)
+            
+    def test_sign_up(self):
+        with patch("server.render_template") as mock_render_template:
+            response = self.server.sign_up()
+            self.assertIsNotNone(response)
+            self.assertTrue(mock_render_template.called)
+            
+    def test_upload(self):
+        with patch("server.render_template") as mock_render_template:
+            response = self.server.upload()
+            self.assertIsNotNone(response)
+            self.assertTrue(mock_render_template.called)
+            
+    def test_coral_info(self):
+        with patch("server.render_template") as mock_render_template:
+            response = self.server.coral_info()
+            self.assertIsNotNone(response)
+            self.assertTrue(mock_render_template.called)
+            
